1d0c5d5d8585186d17d2e9cbc49ebca671eef9b9
StepanNaryshkov <stepan.naryshkov@eleks.com>
2021-10-11 14:29:23 +0300

    try

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
-// eslint-disable-next-line prettier/prettier
-// eslint-disable-next-line prettier/prettier
+// eslint-disable-next-line no-undef
 module.exports = {
   'env': {
+    'node': true,
+    'commonjs': true,
     'browser': true,
-    'es2021': true,
+    'es6': true,
+    'jest': true,
   },
   'extends': [
+    'eslint:recommended',
     'plugin:react/recommended',
     'google',
   ],
@@ -19,19 +22,34 @@ module.exports = {
   },
   'plugins': [
     'react',
+    'react-hooks',
     '@typescript-eslint',
   ],
+  'globals': {
+    'JSX': 'readonly',
+  },
   'rules': {
-    'prettier/prettier': [
-      'error',
-      {
-        'singleQuote': true,
-      },
-    ],
-    'react/jsx-filename-extension': [
+    'react-hooks/exhaustive-deps': 'error',
+    'no-var': 'error',
+    'brace-style': 'error',
+    'prefer-template': 'error',
+    'space-before-blocks': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off',
+    'require-jsdoc': 'off',
+    'max-len': [
       'warn',
       {
-        'extensions': ['.js', '.jsx', '.ts', '.tsx'],
+        'tabWidth': 2,
+        'ignoreComments': false,
+        'ignoreTrailingComments': true,
+        'ignoreUrls': true,
+        'ignoreStrings': true,
+        'ignoreTemplateLiterals': true,
+        'ignoreRegExpLiterals': true,
+        'code': 100,
       },
     ],
   },
